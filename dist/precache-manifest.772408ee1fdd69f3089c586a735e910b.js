8e66f4f5a2f5da720571cd8f24613a0e4908dcff
Igor Paranhos <igor.paranhosto@gmail.com>
2019-10-06 12:21:14 -0300

    Removing files

---
+++ dist/precache-manifest.772408ee1fdd69f3089c586a735e910b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "aebfcea955d96f957ded",
+    "url": "/js/chunk-vendors.0c9f89b2.js"
+  },
+  {
+    "revision": "a80feae9a8beba1e5ee9",
+    "url": "/js/app.3ebd4464.js"
+  },
+  {
+    "revision": "cde54e71406190a7cebd34d1338587e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a80feae9a8beba1e5ee9",
+    "url": "/css/app.d5e438db.css"
+  }
+];
